08ecb9a2de58b3d25a5693561eb3a20474c87f85
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-12-17 16:45:10 +0100

    chore: setup eslint rule demo in next-js-app + tests

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,9 +1,17 @@
-// require('ts-node').register();
-
+import type { ESLint } from 'eslint'
+import { name, version } from '../package.json'
 import myFirstRule from './rules/my-first-rule'
 
-const rules = {
-  'my-first-rule': myFirstRule,
-}
+const plugin = {
+  // https://eslint.org/docs/latest/extend/plugins#meta-data-in-plugins
+  // @ts-expect-error
+  meta: {
+    name,
+    version,
+  },
+  rules: {
+    'my-first-rule': myFirstRule,
+  },
+} satisfies ESLint.Plugin
 
-export default rules
+export default plugin
