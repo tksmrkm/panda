33e2619e864ef2b161971c5bf1e262f7d7c3921a
anubra266 <anubra266@gmail.com>
2023-12-20 22:05:29 +0100

    reverts

--- sandbox/vite-ts/vite.config.ts
+++ sandbox/vite-ts/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
 
 const ANALYZE = !!process.env.ANALYZE
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   build: {
     sourcemap: ANALYZE,
   },
