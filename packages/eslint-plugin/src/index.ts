bf9fc7a17953bcd485814ca2b0b575030bc055fe
anubra266 <anubra266@gmail.com>
2023-12-20 23:09:14 +0100

    cleanup

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,9 +1,7 @@
 import { name, version } from '../package.json'
-import myFirstRule, { RULE_NAME as MyFirstRule } from './rules/my-first-rule'
 import noDebug, { RULE_NAME as NoDebug } from './rules/no-debug'
 
 export const rules = {
-  [MyFirstRule]: myFirstRule,
   [NoDebug]: noDebug,
 } as any
 
--- packages/eslint-plugin/src/rules/no-debug.test.ts
+++ packages/eslint-plugin/src/rules/no-debug.test.ts
@@ -3,52 +3,48 @@ import rule, { RULE_NAME } from './no-debug'
 
 const ruleTester = new RuleTester()
 
-const valid = [
-  { code: 'const styles = css({ bg: "red" })' },
+const parserOptions = {
+  ecmaFeatures: {
+    jsx: true,
+  },
+}
 
+const imports = `import { css } from './panda/css'
+import { Circle } from './panda/jsx'
+`
+
+const valids = [
+  'const styles = css({ bg: "red" }) ',
+  // 'const stylesNested2 = css({ bg: "red", "&:hover": { "&:disabled": { debug: true } } })',
   // TODO Ensure that it's only dissalowed within panda styles
-  // { code: 'const randomFunc = f({ debug: true })' },
-
+  //  'const randomFunc = f({ debug: true })',
   // TODO Ensure that it's only dissalowed within panda components
-  //   {
-  //     code: '<NonPandaComponent debug={true} />',
-  //     parserOptions: {
-  //       ecmaFeatures: {
-  //         jsx: true,
-  //       },
-  //     },
-  //   },
+  '<NonPandaComponent debug={true} />',
 ]
 
-const invalid = [
-  {
-    code: 'const styles = css({ bg: "red", debug: true })',
+const invalids = [
+  'const styles = css({ bg: "red", debug: true })',
+  'const styles = css({ bg: "red", "&:hover": { debug: true } })',
+  'const styles = css({ bg: "red", "&:hover": { "&:disabled": { debug: true } } })',
+
+  '<Circle debug={true} />',
+  // '<Circle css={{ debug: true }} />',
+  // '<Circle css={{ "&:hover": { debug: true } }} />'
+]
+
+ruleTester.run(RULE_NAME, rule as any, {
+  valid: valids.map((code) => ({
+    code: imports + code,
+    parserOptions,
+  })),
+  invalid: invalids.map((code) => ({
+    code: imports + code,
     errors: [
       {
         messageId: 'debug',
         suggestions: null,
       },
     ],
-  },
-
-  {
-    code: `import { Circle } from './panda/jsx'
-    <Circle debug={true} />`,
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    errors: [
-      {
-        messageId: 'debugProp',
-        suggestions: null,
-      },
-    ],
-  },
-]
-
-ruleTester.run(RULE_NAME, rule as any, {
-  valid,
-  invalid,
+    parserOptions,
+  })),
 })
