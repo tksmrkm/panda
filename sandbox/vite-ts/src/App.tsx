cc0b8f391711afe5423041a675b779c75a47127c
anubra266 <anubra266@gmail.com>
2023-12-20 22:11:56 +0100

    Remove sandbox changes

--- sandbox/vite-ts/src/App.tsx
+++ sandbox/vite-ts/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <div className={stack({ padding: '40px', align: 'stretch', color: 'red.300' })}>
-      <section className={css({ padding: '5', borderWidth: '1px', debug: true })}>
+      <section className={css({ padding: '5', borderWidth: '1px' })}>
         <p className={css({ fontWeight: 'semibold', mb: '2' })}>CSS - Function</p>
         <div className={css({ maxWidth: '840px', marginX: 'auto', textAlign: 'center' })}>
           <div>
